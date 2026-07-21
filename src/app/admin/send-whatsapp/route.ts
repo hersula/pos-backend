8fc078c6f4a66fab1f46d20368b408b130257d7e
hersula <herman.sulaeman@gmail.com>
2026-07-21 16:38:41 +0700

    Update route.ts

--- src/app/admin/send-whatsapp/route.ts
+++ src/app/admin/send-whatsapp/route.ts
@@ -59,7 +59,7 @@ export async function POST(request: NextRequest) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'X-Device-Id': '969de968-96f2-40e0-a8f0-c6d6ecd96873',
+        'X-Device-Id': '11722d7b-0836-477c-9800-cfc4a12f1731',
         'Authorization': 'Basic ' + Buffer.from('admin:StrongPassword123!').toString('base64')
       },
       body: JSON.stringify({
